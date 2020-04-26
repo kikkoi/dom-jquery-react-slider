defecabb9c99751ebd88e4f1832059eccdebc8f8
Martin Betlem <martin.betlem@gmail.com>
2020-04-26 15:38:41 +0300

    Remove react

--- React/src/components/dot.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export default function Dot(props) {
-    const {index, activeIndex} = props;
-    const cls = `${index===activeIndex ? 'dot active':'dot'}`;
-    return (
-        <div className={cls}></div>
-    )
-}
